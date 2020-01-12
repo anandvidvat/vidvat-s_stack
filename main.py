0aabd49badea641e67da5a28f6cc1fb8b82f57cb
Arza <anand.vidvat.arza@hp.com>
2020-01-12 13:34:53 +0530

    added index route

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template,url_for
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-@app.route('/')
 @app.route('/home')
+@app.route('/index')
 def home():
     return render_template('/index.html')
 
